43343a1757aec5d04d09c9d834883424f50e6f5f
topsy <Malatjimj2@gmail.com>
2025-04-26 01:23:18 +0200

    Feature: Added proper responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <main className="flex flex-row justify-center items-center w-full h-screen bg-cyan-950">
-        <h1 className="text-9xl font-black text-white">ViSiON</h1>
-      </main>
+    <main className="flex flex-col justify-center items-center w-full h-screen bg-cyan-950 px-4">
+      <h1 className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl font-black text-white text-center">ViSiON</h1>
+    </main>
   );
 }
